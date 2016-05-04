5a4e7bb62b31d4be60b98c2005ea425b443ded36
chyvn <chyavana.tul@outlook.com>
2016-05-04 19:54:24 +0530

    Created BST project to work with Binary Search Trees, and finished the JDq submission.

---
+++ src/com/bst/chyn/Node.java
@@ -0,0 +1,68 @@
+package com.bst.chyn;
+
+/**
+ * Created by v-vetula on 5/4/2016.
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    private Node insert(Node root, int data){
+        if(root == null){
+            root = new Node();
+            root.data = data;
+            root.left = null;
+            root.right = null;
+            return root;
+        }
+        else{
+            Node temp = root;
+            if(data > temp.data){
+                if(temp.right != null) insert(temp.right, data);
+                else{
+                    Node newNode = new Node();
+                    newNode.data =data;
+                    temp.right = newNode;
+                    newNode.left = null;
+                    newNode.right = null;
+                }
+            }
+            else{
+                if(temp.left != null) insert(temp.left, data);
+                else{
+                    Node newNode = new Node();
+                    newNode.data =data;
+                    temp.left = newNode;
+                    newNode.left = null;
+                    newNode.right = null;
+                }
+            }
+        }
+        return root;
+    }
+
+    private void Inorder(Node root){
+        if(root != null){
+            if(root.left != null) Inorder(root.left);
+            System.out.print(root.data + " ");
+            if(root.right != null) Inorder(root.right);
+        }
+    }
+
+    private void Preorder(Node root){
+        if(root != null){
+            System.out.print(root.data + " ");
+            if(root.left != null) Preorder(root.left);
+            if(root.right != null) Preorder(root.right);
+        }
+    }
+
+    private void Postorder(Node root){
+        if(root != null){
+            if(root.left != null) Postorder(root.left);
+            if(root.right != null) Postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+}
--- src/com/chyn/JDq.java
+++ src/com/chyn/JDq.java
@@ -8,28 +8,49 @@ import java.util.Scanner;
 public class JDq {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        //reading line 1, values of n and m.
         String line1 = scanner.nextLine();
         int n = Integer.parseInt(line1.split(" ")[0]);
         int m = Integer.parseInt(line1.split(" ")[1]);
+
+        //reading line 2, the array a[n]
         int[] array = new int[n];
         line1 = scanner.nextLine();
         for (int i = 0; i < n; i++) {
             array[i] = Integer.parseInt(line1.split(" ")[i]);
         }
+
         //declare subarray
         int[] subArray = new int[m];
+        int max = 0;
+
+        //iterate over the array till n-m to find max sub array.
         for (int i = 0; i < n - m; i++) {
             int index = 0;
+
+            //create the sub array
             for (int j = i; j < i + m; j++) {
                 subArray[index] = array[j];
                 ++index;
             }
-            for(int j = 0; j<subArray.length; j++){
 
+            //counting number of unique characters in this array.
+            int count = 0;
+            for(int k = 0; k<m; k++){
+                boolean unique = true;
+                for(int j = k+1; j<m; j++){
+                    if(subArray[k] == subArray[j]) unique = false;
+                }
+                if(unique) ++count;
             }
+
+            //updating max unique chars
+            max = count;
+            if(count == m) break;
         }
-        //iterate over main array with subarray times.
-        //check number of unique entries.
-        //if unique is m quit, else keep the next max.
+
+        //printing output.
+        System.out.println(max);
     }
 }
